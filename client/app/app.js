5624b820b581658b59f4ba49b1af0ddec21ec694
Cameron <camdunne@gmail.com>
2017-03-06 12:36:31 -0500

    added angular

--- client/app/app.js
+++ client/app/app.js
@@ -2,20 +2,20 @@ var app = angular.module('app', [
 
 ])
 .controller('todoCtrl', function($scope) {
-  $scope.todoList = [{todoText: 'Clean House', done:false}];
+  $scope.todos = [{text: 'Clean House', done: false}];
 
-  $scope.add = function() {
-    $scope.todoList.push({todoText:$scope.todoInput, done:false});
+  $scope.addTodo = function() {
+    $scope.todos.push({text: $scope.todoInput, done: false});
     $scope.todoInput = "";
   };
 
-  $scope.remove = function(){
-    var oldList = $scioe.todoList;
-    $scope.todoList = [];
-    angular.forEach(oldList, function(item) {
-      if(!item.done) $scope.todoList.push(item);
+  $scope.removeTodo = function(){
+    var oldList = $scope.todos;
+    $scope.todos = [];
+    angular.forEach(oldList, function(todo) {
+      if(!todo.done) $scope.todos.push(todo);
     });
   };
 });
 
-module.export=app;
+module.exports = app;
